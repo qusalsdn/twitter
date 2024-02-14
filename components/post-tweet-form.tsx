034d46872f9b496ad59245fc0f898333baac749e
변민우 <yja05092@gmail.com>
2024-02-14 21:00:53 +0900

    Uploading Images

--- components/post-tweet-form.tsx
+++ components/post-tweet-form.tsx
@@ -1,12 +1,13 @@
-import { auth, db } from "@/src/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { auth, db, storage } from "@/src/firebase";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Image from "next/image";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface Tweet {
   tweet: string;
-  imageFile: any[];
+  imageFile: any;
 }
 
 export default function PostTweetForm() {
@@ -28,18 +29,30 @@ export default function PostTweetForm() {
   };
 
   const onSubmit = async (formData: Tweet) => {
-    console.log(formData);
     const user = auth.currentUser;
     if (!user || loading || formData.tweet === "" || formData.tweet.length > 256) return;
+    if (formData.imageFile[0].size > 1024 ** 2)
+      return alert("이미지 크기의 용량은 1MB 이하여야 합니다.");
     try {
       setLoading(true);
-      await addDoc(collection(db, "tweets"), {
+      const doc = await addDoc(collection(db, "tweets"), {
         tweet: formData.tweet,
         createdAt: Date.now(),
         userName: user.displayName || "익명",
         userId: user.uid,
       });
+      if (formData.imageFile) {
+        // 스토리지에 사진 저장
+        const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`);
+        // 저장된 사진의 결과값 result에 저장
+        const result = await uploadBytes(locationRef, formData.imageFile[0]);
+        // 저장된 사진의 url을 추출
+        const url = await getDownloadURL(result.ref);
+        // Firestore Database에 저장된 doc의 값을 url을 추가하여 업데이트
+        await updateDoc(doc, { photo: url });
+      }
       reset();
+      setImageSrc(null);
     } catch (e) {
       console.error(e);
     } finally {
@@ -53,6 +66,7 @@ export default function PostTweetForm() {
         placeholder="트윗을 입력해주세요."
         className="border-2 border-white p-5 rounded-3xl text-lg text-white bg-black w-full h-40 resize-none focus:outline-none focus:border-sky-500 duration-300"
         maxLength={256}
+        required
         {...register("tweet")}
       ></textarea>
       {imageSrc !== null && (
