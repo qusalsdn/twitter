4142411d9544b413a35ee92e546f912e6b01b5f2
변민우 <yja05092@gmail.com>
2024-02-17 13:58:45 +0900

    Changing the User Name

--- app/(loginRequired)/profile/page.tsx
+++ app/(loginRequired)/profile/page.tsx
@@ -4,20 +4,35 @@ import { ITweet } from "@/components/timeline";
 import Tweet from "@/components/tweet";
 import { auth, db, storage } from "@/src/firebase";
 import { updateProfile } from "firebase/auth";
-import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface FormData {
+  userName: string;
+  imageFile: any;
+}
+
 export default function Profile() {
   const user = auth.currentUser;
   const router = useRouter();
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue } = useForm<FormData>();
   const [loginCheck, setLoginCheck] = useState(false);
   const [avatar, setAvatar] = useState(user?.photoURL);
   const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [inputVisible, setInputVisible] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const fetchTweets = async () => {
@@ -61,6 +76,30 @@ export default function Profile() {
     }
   };
 
+  const onSubmitUserNameChange = async (formData: FormData) => {
+    if (!user) return;
+    if (formData.userName.length > 15) return alert("15자 이하로 작성해주세요.");
+    try {
+      setLoading(true);
+      await updateProfile(user, { displayName: formData.userName });
+      // 아래는 트윗의 userName을 변경하기 위해 작성
+      const tweetsQuery = query(collection(db, "tweets"), where("userId", "==", user.uid));
+      const snapShop = await getDocs(tweetsQuery);
+      const docRefs = snapShop.docs.map((doc) => {
+        return doc.ref;
+      });
+      for (const ref of docRefs) {
+        await updateDoc(ref, { userName: formData.userName });
+      }
+      setInputVisible(false);
+      router.refresh();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-full">
       {loginCheck && (
@@ -97,9 +136,54 @@ export default function Profile() {
               className="hidden"
               {...register("imageFile", { onChange: onChangeAvatar })}
             />
-            <p className="text-lg font-bold">{user?.displayName ?? "익명"}</p>
+            {inputVisible ? (
+              <form
+                onSubmit={handleSubmit(onSubmitUserNameChange)}
+                className="flex flex-col space-y-2 items-center"
+              >
+                <input
+                  type="text"
+                  {...register("userName")}
+                  className="outline-none rounded-3xl text-black text-center py-2 font-bold pr-2"
+                />
+                {loading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <div className="space-x-3">
+                    <button
+                      type="button"
+                      className="bg-red-500 py-2 px-5 rounded-3xl font-bold"
+                      onClick={() => setInputVisible(false)}
+                    >
+                      취소
+                    </button>
+                    <button type="submit" className="bg-[#0ea5e9] py-2 px-5 rounded-3xl font-bold">
+                      저장
+                    </button>
+                  </div>
+                )}
+              </form>
+            ) : (
+              <div className="flex flex-col items-center space-y-2">
+                <p className="text-lg font-bold">{user?.displayName ?? "익명"}</p>
+                <button
+                  title="이름 변경"
+                  onClick={() => {
+                    setInputVisible(true);
+                    user?.displayName ? setValue("userName", user.displayName) : "";
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5">
+                    <path
+                      fill="#ffffff"
+                      d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
           </div>
-          <div className="space-y-10 h-[700px] scrollBar px-5">
+          <div className="space-y-10 h-[670px] scrollBar px-5">
             {loading ? (
               <h1>Loading...</h1>
             ) : (
