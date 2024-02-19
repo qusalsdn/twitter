42d8a8c336b79f6ff78040b497feb727e634a6f4
변민우 <yja05092@gmail.com>
2024-02-19 13:33:35 +0900

    hide navbar

--- app/(loginRequired)/layout.tsx
+++ app/(loginRequired)/layout.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Navbar from "@/components/navbar";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [loginCheck, setLoginCheck] = useState(false);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) setLoginCheck(true);
+  });
+
   return (
     <div className="flex mt-16 space-x-16">
-      <Navbar />
+      {loginCheck && <Navbar />}
       {children}
     </div>
   );
